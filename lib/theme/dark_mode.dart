5235437f4e04377eef5552cde792218bba62846d
India Heath <n00213565@iadt.ie>
2025-03-09 20:34:31 +0000

    updated colorScheme to reflect UI design colors

--- lib/theme/dark_mode.dart
+++ lib/theme/dark_mode.dart
@@ -3,13 +3,19 @@ import 'package:flutter/material.dart';
 ThemeData darkMode = ThemeData(
   brightness: Brightness.dark,
   colorScheme: ColorScheme.dark(
-    surface: Colors.grey.shade900,
-    primary: Colors.grey.shade800,
-    secondary: Colors.grey.shade600,
-    inversePrimary: Colors.grey.shade300,
+    surface: Color(0xFF121212),
+    // surface: Colors.grey.shade900,
+    primary: Color(0xFFFFFFFF),
+    // primary: Colors.grey.shade800,
+    secondary: Color(0xFF777777),
+    tertiary: Color(0xFF1ED760),
+    // secondary: Colors.grey.shade600,
+    inversePrimary: Color(0xFF121212),
+    // inversePrimary: Colors.grey.shade300,
   ),
   textTheme: ThemeData.dark().textTheme.apply(
-        bodyColor: Colors.grey[200],
+        bodyColor: Color(0xFFB3B3B3),
+        // bodyColor: Colors.grey[200],
         displayColor: Colors.white,
       ),
   // More light theme properties here
